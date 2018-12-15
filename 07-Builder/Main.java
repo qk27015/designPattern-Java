31facb8fc73b5c3cb6292451ff4cae803e00feb4
qk27015 <tk5760a@gmail.com>
2018-12-16 08:05:39 +0900

    Builder

---
+++ 07-Builder/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    TextBuilder textBuilder = new TextBuilder();
+    Director director = new Director(textBuilder);
+    director.construct();
+    String result = textBuilder.getResult();
+    System.out.println(result);
+  }
+}
